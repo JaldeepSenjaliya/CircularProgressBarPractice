9befd654f2096b8a45de08a525d39f89ab5995ee
simranjots <simranjot.bagga8@gmail.com>
2021-06-07 23:03:18 -0400

    Initial Commit

---
+++ TestCircularProgressBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCircularProgressBar
+//
+//  Created by Jaldeep Patel on 2021-06-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
